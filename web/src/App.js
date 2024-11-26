b7c90484d95c7fd28d02344d3d2ada1092639fb2
tanubhavjuneja <119427827+tanubhavjuneja@users.noreply.github.com>
2024-11-26 16:50:13 +0530

    .

--- web/src/App.js
+++ web/src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Events from './pages/Events';
 import Sponsors from './pages/Sponsors';
-import EventDetail from './pages/EventDetail';
-import SponsorDetail from './pages/SponsorDetail'; // Import the SponsorDetail component
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import {
@@ -20,7 +18,6 @@ import {
   faPhone,
   faArrowRight,
 } from '@fortawesome/free-solid-svg-icons';
-
 library.add(
   fab,
   faCheckSquare,
@@ -43,11 +40,6 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/events" element={<Events />} />
         <Route path="/sponsors" element={<Sponsors />} />
-        <Route path="/events/:id" element={<EventDetail />} />
-        <Route
-          path="/sponsors/:id" // Add the route for SponsorDetail
-          element={<SponsorDetail />}
-        />
       </Routes>
     </Router>
   );
